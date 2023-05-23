4fafa4ea5b811273b12a0b143c1aad0a1824b4cf
alex <turdzeladze@gmail.com>
2023-05-23 15:30:30 +0400

    retrieving unmodified hackernews pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 LOCAL_HOST = '127.0.0.1'
 LOCAL_PORT = 8080
@@ -6,6 +7,15 @@ LOCAL_PORT = 8080
 TARGET_HOST = 'https://news.ycombinator.com'
 
 app = Flask(__name__)
+
+
+@app.route('/', defaults={'s': ''})
+@app.route('/<string:s>')
+def home(s):
+    suffix = request.url.split(str(LOCAL_PORT))[1]
+    response = requests.get(TARGET_HOST + suffix)
+
+    return response.content, response.status_code
 
 
 if __name__ == '__main__':
